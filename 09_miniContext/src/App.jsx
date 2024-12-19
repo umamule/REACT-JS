8ec477af486b4a02fb84e74a01667e55f8144903
umamule <158204542+umamule@users.noreply.github.com>
2024-12-19 22:34:50 +0530

    context api project learn

---
+++ 09_miniContext/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './component/Login'
+import Profile from './component/Profile'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+     <h1>Uma</h1>
+     <Login/>
+     <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
