ef5c8e3f3472acc143f37f48144dda1d3148a188
umamule <158204542+umamule@users.noreply.github.com>
2024-12-20 20:10:29 +0530

    todo

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import {TodoProvider} from './context'
+import {TodoProvider} from './contexts'
 import './App.css'
 import TodoForm from './components/TodoForm'
 import TodoItem from './components/TodoItem'
--- 10todocontextLocal/src/contexts/TodoContext.js
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -5,7 +5,7 @@ export const TodoContext = createContext({
         {
             id: 1,
             todo: " Todo msg",
-            completed: true,
+            completed: false,
         }
     ],
     addTodo: (todo) => {},
