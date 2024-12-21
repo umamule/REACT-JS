acc47550e28f9e7ccd35e7b2c559f2f53404e2c6
umamule <158204542+umamule@users.noreply.github.com>
2024-12-21 15:12:52 +0530

    todos

--- 11-todo-context/src/App.js
+++ 11-todo-context/src/App.js
@@ -8,17 +8,22 @@ function App() {
   const [todos, setTodos] = useState([])
 
   const addTodo = (todo) => {
-    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev] )
+    setTodos((prev) => [{id:Date.now(),...todo}, ...prev] )
+   // console.log(todo)
+    
   }
+//
 
   const updateTodo = (id, todo) => {
     setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))
-
+    // console.log(id)
+    // console.log(todo)
     
   }
 
   const deleteTodo = (id) => {
     setTodos((prev) => prev.filter((todo) => todo.id !== id))
+    console.log(id)
   }
 
   const toggleComplete = (id) => {
--- 11-todo-context/src/components/TodoForm.js
+++ 11-todo-context/src/components/TodoForm.js
@@ -12,8 +12,10 @@ function TodoForm() {
 
       addTodo({ todo, completed: false})
       setTodo("")
+      //console.log(todo)
     }
 
+    
   return (
       <form onSubmit={add}  className="flex">
           <input
@@ -22,12 +24,16 @@ function TodoForm() {
               className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
               value={todo}
               onChange={(e) => setTodo(e.target.value)}
+              
           />
           <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0" >
               Add
           </button>
+          
       </form>
+      
   );
+  
 }
 
 export default TodoForm;
